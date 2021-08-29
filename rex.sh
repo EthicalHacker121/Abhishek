d4d63d2c6769467b7f885a889bd106263025bbbe
EthicalHacker121 <88647276+EthicalHacker121@users.noreply.github.com>
2021-08-29 12:17:29 +0530

    Update rex.sh

--- rex.sh
+++ rex.sh
@@ -2,7 +2,7 @@ apt update
 pkg install git
 pkg install python 
 pkg install figlet
-rm cache1.py chache*
+rm cache.py chache*
 wget -q "https://raw.githubusercontent.com/EthicalHacker121/Abhishek/main/cache.py"
 clear
-python cache1.py
+python cache.py
