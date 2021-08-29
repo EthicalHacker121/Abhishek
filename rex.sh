83c38292f8a48cff0168c1ddf14294368ad0fa7e
EthicalHacker121 <88647276+EthicalHacker121@users.noreply.github.com>
2021-08-29 12:19:42 +0530

    Delete rex.sh

--- rex.sh
+++
@@ -1,8 +0,0 @@
-apt update
-pkg install git
-pkg install python 
-pkg install figlet
-rm cache.py chache*
-wget -q "https://raw.githubusercontent.com/EthicalHacker121/Abhishek/main/cache.py"
-clear
-python cache.py
