cbe1ff1b739f0ef82c4fe0c7f7e4cf2cf26dd446
EthicalHacker121 <88647276+EthicalHacker121@users.noreply.github.com>
2021-08-31 10:22:34 +0530

    Update cache.py

--- cache.py
+++ cache.py
@@ -70,46 +70,7 @@ def hero():
     while(y):
         clear()
         system(banner)
-        print("Contact me on telegram @Prithvihackz")
-        print("Enter ",newperiod," Parity Price :")
-        current=input()
-        current=int(current)
-        chalo()
-        print("\n---------Successfully hacked the server-----------")
-        chalo1()
-        print("\n---------Successfully got the colour -------------")
-        print('\n')
-    	def getSum(n):
-                sum = 0
-           	    for digit in str(n):
-           	        sum += int(digit)
-           	    return sum
-
-    	if(i in thisway):
-    		m=getSum(current)
-    		n=int(current)%10
-    		if((m%2==0 and n%2==0) or (m%2==1 and n%2==1)):
-    			print(current ,": G",m,n,i,"sum")
-    		else:
-    			print(current ,": R",m,n,i,"sum")
-    	elif(i in thatway):
-    		m=getSum(current)+1
-    		n=int(current)%10
-    		if((m%2==0 and n%2==0) or (m%2==1 and n%2==1)):
-    			print(current ,": G",m,n,i,"sum+1")
-    		else:
-    			print(current ,": R",m,n,i,"Sum+1")
-    	i=i+1
-        decision+=1
-        numbers.append(current)
-        y=input("Do you want to play : Press 1 and 0 to exit \n")
-        if (len(numbers)>11):
-            clear()
-            system('figlet Thank you!!')
-            print("Play on next specified time!!")
-            print("-----------Current Time UP----------")
-            sys.exit(" \n \n \n Contact on Telegram @Prithvihackz")
-            #print(numbers)
+       
 
     
 if(expirydate>today):
