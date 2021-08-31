17f9e22d960a608e7af627c97f7a6c56fbba5519
EthicalHacker121 <88647276+EthicalHacker121@users.noreply.github.com>
2021-08-31 14:39:48 +0530

    Update cache.py

--- cache.py
+++ cache.py
@@ -65,60 +65,7 @@ def hero():
     y=1
     newperiod=period
     banner='figlet RXCE'
-    m=0
-    i=1
-    thisway=[1,3,4,5,6,7,8,10,11,14,19,20]
-    thatway=[2,9,12,13,15,16,17,18]
-    numbers=[]
-    while(y):
-        clear()
-        system(banner)
-        print("Contact me on telegram @Prithvihackz")
-        print("Enter ",newperiod," Parity Price :")
-        current=input()
-        current=int(current)
-        chalo()
-        print("\n---------Successfully hacked the server-----------")
-        chalo1()
-        print("\n---------Successfully got the colour -------------")
-        print('\n')
-        def getSum(n):
-            sum=0
-            for digit in str(n):
-                sum += int(digit)
-            return sum
-        if i in thisway:
-            m=getSum(current)
-            n=int(current)%10
-            if((m%2==0 and n%2==0) or (m%2==1 and n%2==1)):
-                if current in numbers:
-                    print(newperiod+1," : RED")
-                else:
-                    print(newperiod+1," : GREEN")
-            else:
-                if current in numbers:
-                    print(newperiod+1," : GREEN")
-                else:
-                    print(newperiod+1," : RED")
-        if i in thatway:
-            m=getSum(current)+1
-            n=int(current)%10
-            if((m%2==0 and n%2==0) or (m%2==1 and n%2==1)):
-                if current in numbers:
-                    print(newperiod+1,": RED")
-                else:
-                    print(newperiod+1,": GREEN")
-            else:
-                if current in numbers:
-                    print(newperiod+1,": GREEN")
-                else:
-                    print(newperiod+1,": RED")
-        i=i+1
-        newperiod+=1
-        numbers.append(current)
-        y=input("Do you want to play : Press 1 and 0 to exit \n")
-        if(y==0):
-            y=False
+  
         if (len(numbers)>11):
             clear()
             system('figlet Thank you!!')
