4ccec30e3f9826110c92952ddc92e1b37333b786
EthicalHacker121 <88647276+EthicalHacker121@users.noreply.github.com>
2021-08-31 13:48:02 +0530

    Update cache.py

--- cache.py
+++ cache.py
@@ -14,7 +14,7 @@ expirydate = datetime.date(2021, 9, 1)
 #expirydate = datetime.date(2021, 8, 30)
 today=date.today()
 def hero():
-    
+
     def chalo():
         done = False
         #here is the animation
@@ -62,17 +62,72 @@ def hero():
             _ = system('clear')
 
     clear()
-    decision=1
     y=1
     newperiod=period
     banner='figlet RXCE'
+    m=0
+    i=1
+    thisway=[1,3,4,5,6,7,8,10,11,14,19,20]
+    thatway=[2,9,12,13,15,16,17,18]
     numbers=[]
     while(y):
         clear()
         system(banner)
-       
+        print("Contact me on telegram @Prithvihackz")
+        print("Enter ",newperiod," Parity Price :")
+        current=input()
+        current=int(current)
+        chalo()
+        print("\n---------Successfully hacked the server-----------")
+        chalo1()
+        print("\n---------Successfully got the colour -------------")
+        print('\n')
+        def getSum(n):
+            sum=0
+            for digit in str(n):
+                sum += int(digit)
+            return sum
+        if i in thisway:
+            m=getSum(current)
+            n=int(current)%10
+            if((m%2==0 and n%2==0) or (m%2==1 and n%2==1)):
+                if current in numbers:
+                    print(newperiod+1," : RED")
+                else:
+                    print(newperiod+1," : GREEN")
+            else:
+                if current in numbers:
+                    print(newperiod+1," : GREEN")
+                else:
+                    print(newperiod+1," : RED")
+        if i in thatway:
+            m=getSum(current)+1
+            n=int(current)%10
+            if((m%2==0 and n%2==0) or (m%2==1 and n%2==1)):
+                if current in numbers:
+                    print(newperiod+1,": RED")
+                else:
+                    print(newperiod+1,": GREEN")
+            else:
+                if current in numbers:
+                    print(newperiod+1,": GREEN")
+                else:
+                    print(newperiod+1,": RED")
+        i=i+1
+        newperiod+=1
+        numbers.append(current)
+        y=input("Do you want to play : Press 1 and 0 to exit \n")
+        if(y==0):
+            y=False
+        if (len(numbers)>11):
+            clear()
+            system('figlet Thank you!!')
+            print("Play on next specified time!!")
+            print("-----------Current Time UP----------")
+            sys.exit(" \n \n \n Contact on Telegram @Prithvihackz")
+            #print(numbers)
 
-    
+
 if(expirydate>today):
     now = datetime.datetime.now()
     First = now.replace(hour=13, minute=55, second=0, microsecond=0)
@@ -84,11 +139,11 @@ if(expirydate>today):
     Final = now.replace(hour=17, minute=55, second=0, microsecond=0)
     Finalend = now.replace(hour=18, minute=35, second=0, microsecond=0)
 
-    if (now>First and now<Firstend):
-            period=280
+    if (False):
+            period=220
             hero()
-    elif(now>Second and now<Secondend):
-            period=320
+    elif(now>First and now<Firstend):
+            period=280
             hero()
     elif(now>Third and now<Thirdend):
             period=340
